1d3b08946470d8ec40120f8fd2c544c03d96fada
ChernyshovNikita <workxrom@gmail.com>
2022-03-27 18:07:25 +0300

    feat: filters in table by Asset name

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,16 +1,7 @@
-import {Fragment, useEffect, useState} from 'react';
+import {Fragment, memo, useCallback, useEffect, useState} from 'react';
 import {Combobox, Transition} from '@headlessui/react';
 import {CheckIcon, SelectorIcon} from '@heroicons/react/solid';
 import {StoreType} from '../hooks';
-
-const defaultItems: OptionsType = [
-  {id: 1, value: 'Wade Cooper'},
-  {id: 2, value: 'Arlene Mccoy'},
-  {id: 3, value: 'Devon Webb'},
-  {id: 4, value: 'Tom Cook'},
-  {id: 5, value: 'Tanya Fox'},
-  {id: 6, value: 'Hellen Schmidt'},
-];
 
 type OptionType = {
   id: number;
@@ -22,125 +13,154 @@ export type AutocompoleteProps = {
   items: StoreType['assets'];
   initSymbol?: string;
   setSymbol?: (val: string) => void;
+  isAlwaysSearching?: boolean;
 };
 
-export const Autocompolete = ({
-  items,
-  initSymbol,
-  setSymbol,
-}: AutocompoleteProps) => {
-  const [options, setOptions] = useState<OptionsType>([]);
-  const [selected, setSelected] = useState<OptionType>();
-  const [search, setSearch] = useState('');
+export const Autocompolete = memo(
+  ({
+    items,
+    initSymbol,
+    setSymbol,
+    isAlwaysSearching = false,
+  }: AutocompoleteProps) => {
+    const [options, setOptions] = useState<OptionsType>([]);
+    const [selected, setSelected] = useState<OptionType>();
+    const [search, setSearch] = useState('');
 
-  useEffect(() => {
-    if (!items) {
-      return;
-    }
+    useEffect(() => {
+      console.log('initSymbol', initSymbol);
 
-    const newOptions = items?.map((value, index) => ({id: index, value}));
-
-    setOptions(newOptions);
-
-    newOptions.forEach(option => {
-      if (option.value === initSymbol) {
-        setSelected(option);
+      if (initSymbol) {
+        setSearch(initSymbol);
       }
-    });
-  }, [items, initSymbol]);
+    }, [initSymbol]);
 
-  useEffect(() => {
-    if (selected?.value && setSymbol) {
-      setSymbol(selected?.value);
-    }
-  }, [selected?.value, setSymbol]);
+    useEffect(() => {
+      if (!items) {
+        return;
+      }
 
-  const filteredList =
-    search === ''
-      ? options
-      : options?.filter(item =>
-          item.value
-            .toLowerCase()
-            .replace(/\s+/g, '')
-            .includes(search.toLowerCase().replace(/\s+/g, '')),
-        );
+      const newOptions = items?.map((value, index) => ({id: index, value}));
 
-  return (
-    <div className="relative">
-      <Combobox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
-          <div
-            className={`
+      setOptions(newOptions);
+
+      newOptions.forEach(option => {
+        if (option.value === initSymbol) {
+          setSelected(option);
+        }
+      });
+    }, [items, initSymbol]);
+
+    useEffect(() => {
+      if (selected?.value && setSymbol) {
+        setSymbol(selected?.value);
+      }
+    }, [selected?.value, setSymbol]);
+
+    const onSearchChange = useCallback(
+      event => {
+        const val = event?.target?.value;
+
+        setSearch(val);
+        if (isAlwaysSearching && setSymbol) {
+          setSymbol(val);
+        }
+      },
+      [isAlwaysSearching, setSymbol],
+    );
+
+    const filteredList =
+      search === ''
+        ? options
+        : options?.filter(item =>
+            item.value
+              .toLowerCase()
+              .replace(/\s+/g, '')
+              .includes(search.toLowerCase().replace(/\s+/g, '')),
+          );
+
+    return (
+      <div className="relative">
+        <Combobox value={selected} onChange={setSelected}>
+          <div className="relative mt-1">
+            <div
+              className={`
               relative w-full 
               text-left bg-sky-400 rounded-lg shadow-md 
               cursor-default focus:outline-none focus-visible:ring-2 
               focus-visible:ring-white focus-visible:ring-offset-teal-300 focus-visible:ring-offset-2 
               sm:text-sm overflow-hidden`}>
-            <Combobox.Input
-              className="w-full border-none focus:ring-0 focus:outline-none py-2 pl-3 pr-10 text-2xl sm:text-3xl font-medium leading-5 text-sky-900"
-              displayValue={(person: OptionType) => person?.value}
-              onChange={event => setSearch(event?.target?.value)}
-            />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <SelectorIcon
-                className="w-8 h-8 text-sky-500"
-                aria-hidden="true"
+              <Combobox.Input
+                className="w-full border-none focus:ring-0 focus:outline-none py-2 pl-3 pr-10 text-2xl sm:text-3xl font-medium leading-5 text-sky-900"
+                displayValue={(person: OptionType) =>
+                  isAlwaysSearching ? search : person?.value
+                }
+                onChange={onSearchChange}
               />
-            </Combobox.Button>
-          </div>
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                <SelectorIcon
+                  className="w-8 h-8 text-sky-500"
+                  aria-hidden="true"
+                />
+              </Combobox.Button>
+            </div>
 
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            afterLeave={() => setSearch('')}>
-            <Combobox.Options
-              className={`absolute z-40 w-full
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              afterLeave={() => setSearch('')}>
+              <Combobox.Options
+                className={`absolute z-40 w-full
               py-1 mt-1 overflow-auto 
               text-base bg-white rounded-md shadow-lg shadow-cyan-400 max-h-60 
               ring-1 ring-cyan-800
               focus:outline-none 
               sm:text-sm`}>
-              {filteredList.length === 0 && search !== '' ? (
-                <div className="cursor-default select-none ring-1 ring-black relative py-2 px-4 text-gray-700">
-                  Nothing found.
-                </div>
-              ) : (
-                filteredList?.map(item => (
-                  <Combobox.Option
-                    key={item?.id}
-                    className={({active}) =>
-                      `cursor-default select-none relative py-2 pl-10 pr-4 ${
-                        active ? 'text-white bg-cyan-600' : 'text-gray-900'
-                      }`
-                    }
-                    value={item}>
-                    {({selected, active}) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
-                          }`}>
-                          {item?.value}
-                        </span>
-                        {selected ? (
+                {filteredList.length === 0 && search !== '' ? (
+                  <div className="cursor-default select-none ring-1 ring-black relative py-2 px-4 text-gray-700">
+                    Nothing found.
+                  </div>
+                ) : (
+                  filteredList?.map(item => (
+                    <Combobox.Option
+                      key={item?.id}
+                      className={({active}) =>
+                        `cursor-default select-none relative py-2 pl-10 pr-4 ${
+                          active ? 'text-white bg-cyan-600' : 'text-gray-900'
+                        }`
+                      }
+                      value={item}>
+                      {({selected, active}) => (
+                        <>
                           <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? 'text-white' : 'text-cyan-600'
+                            className={`block truncate ${
+                              selected ? 'font-medium' : 'font-normal'
                             }`}>
-                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                            {item?.value}
                           </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Combobox.Option>
-                ))
-              )}
-            </Combobox.Options>
-          </Transition>
-        </div>
-      </Combobox>
-    </div>
-  );
-};
+                          {selected ? (
+                            <span
+                              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                active ? 'text-white' : 'text-cyan-600'
+                              }`}>
+                              <CheckIcon
+                                className="w-5 h-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Combobox.Option>
+                  ))
+                )}
+              </Combobox.Options>
+            </Transition>
+          </div>
+        </Combobox>
+      </div>
+    );
+  },
+);
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,10 @@
 import {memo, useCallback, useEffect, useState} from 'react';
 import {AssetsListType} from '../hooks';
 import {theme} from '../theme';
+import {Autocompolete} from './Autocomplete';
 import {PopoverBuySell} from './Popover';
 
-export type TableHeaderType = Array<string>;
+export type TableHeaderType = Array<{name: string; filterId?: string}>;
 
 export type TableProps = {
   header?: TableHeaderType;
@@ -11,11 +12,11 @@ export type TableProps = {
 };
 
 export const headerDefault: TableHeaderType = [
-  '#',
-  'Name',
-  'Icon',
-  'Price',
-  'Actions',
+  {name: '#'},
+  {name: 'Name', filterId: 'name'},
+  {name: 'Icon', filterId: 'icon'},
+  {name: 'Price', filterId: 'price'},
+  {name: 'Actions'},
 ];
 
 const cellClass =
@@ -26,43 +27,121 @@ const boldCellClass =
 export const Table = memo(({header = headerDefault, list = []}: TableProps) => {
   console.info('Render: Table');
 
-  const [shortList, setShortList] = useState<TableProps['list']>();
   const [isShortList, setIsShortList] = useState(true);
+  const [filteredList, setFilteredList] = useState<TableProps['list']>();
+  const [filters, setFilters] = useState<{[key: string]: Array<string>}>({});
+  const [inputValues, setInputValues] = useState<{[key: string]: string}>({
+    name: 'AED',
+  });
 
+  // Init list
   useEffect(() => {
-    setShortList(list.slice(0, 10));
+    console.log('UE, 3');
+    setFilteredList(list.slice(0, 10));
+
+    const initFilterList = Object.keys(list[0] || {}).reduce(
+      (accum, key) => ({...accum, [key]: []}),
+      {},
+    );
+
+    // Filters search list
+    const filterList = list.reduce((accum: any, item: any) => {
+      Object.keys(item).forEach(key => {
+        accum[key].push(String(item[key]));
+      });
+
+      return accum;
+    }, initFilterList);
+
+    setFilters(filterList);
+
+    // Filters value
+    const initFilterInputs = Object.keys(list[0] || {}).reduce(
+      (accum, key) => ({...accum, [key]: ''}),
+      {},
+    );
+
+    setInputValues(initFilterInputs);
   }, [list]);
 
+  const inputValueName = inputValues?.name || '';
+
+  // Apply filters to the list
+  useEffect(() => {
+    setFilteredList(() => {
+      const innerFiltered = list?.filter(
+        ({name}) =>
+          (String(name).match(inputValueName.toUpperCase()) as any)?.index >= 0,
+      );
+
+      if (isShortList) {
+        return innerFiltered.slice(0, 10);
+      }
+
+      return innerFiltered;
+    });
+  }, [inputValueName, isShortList, list]);
+
+  // On toggle short list
+  useEffect(() => {
+    if (isShortList) {
+      setFilteredList(list.slice(0, 10));
+    } else {
+      setFilteredList(list);
+    }
+  }, [isShortList, list]);
+
   const toggleExpand = useCallback(() => setIsShortList(s => !s), []);
+
+  const onChangeFilterValue = useCallback(
+    (key: string) => (val: string) => {
+      return setInputValues(s =>
+        s[key] === val
+          ? s
+          : {
+              ...s,
+              [key]: val,
+            },
+      );
+    },
+    [],
+  );
 
   return (
     <div className="relative rounded-xl flex flex-1 flex-col w-min-screen">
       <table className="min-w-full">
         <thead>
           <tr>
-            {header.map((name, index: number) => (
+            {header.map(({name, filterId}, index: number) => (
               <th key={`${index}${name}`} scope="col" className={boldCellClass}>
                 {name}
+
+                {filterId && (
+                  <Autocompolete
+                    isAlwaysSearching
+                    initSymbol={inputValues[filterId]}
+                    setSymbol={onChangeFilterValue(filterId)}
+                    items={filters[filterId]}
+                  />
+                )}
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {(isShortList ? shortList : list)?.map(
-            ({name, price, icon}, index) => (
-              <tr
-                key={`${index}${name}`}
-                className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                <td className={boldCellClass}>{index}</td>
-                <td className={cellClass}>{name}</td>
-                <td className={cellClass}>{icon}</td>
-                <td className={cellClass}>{price}</td>
-                <td className={cellClass}>
-                  <PopoverBuySell />
-                </td>
-              </tr>
-            ),
-          )}
+          {filteredList?.map(({name, price, icon}, index) => (
+            <tr
+              key={`${index}${name}`}
+              className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+              <td className={boldCellClass}>{index}</td>
+              <td className={cellClass}>{name}</td>
+              <td className={cellClass}>{icon}</td>
+              <td className={cellClass}>{price}</td>
+              <td className={cellClass}>
+                <PopoverBuySell />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
 
