52b3c3b01e6e01f586409f68317ccc0baa762d6f
Chernyshov_Nikita <xpom5566@mail.ru>
2022-06-25 03:35:56 +0300

    fix: button styles finally!

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,36 +1,5 @@
 const buttonCommon = `inline-flex justify-center items-center px-2 py-1 sm:px-4 sm:py-2 text-lg font-medium `;
 
-const getButtonStyle = ({
-  textColor = "white",
-  bgColor = "neutral-800",
-  hover = "",
-  hoverRing = "",
-  custom = "",
-}: {
-  textColor: string;
-  bgColor: string;
-  hover: string;
-  hoverRing: string;
-  custom?: string;
-  loading?: string; // TODO: loading state
-}) => `
-${buttonCommon}
-
-text-${textColor}  
-
-bg-${bgColor} 
-${hover} 
-
-focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 
-
-border-0 rounded-xl 
-focus-visible:ring-${hoverRing} 
-
-active:ring-2 ring-${hoverRing}
-
-${custom}
-`;
-
 export const colors = {
   primary: "sky-400",
   secondary: "gray-500",
@@ -53,39 +22,71 @@ export const bg = {
 
 export const theme = {
   button: {
-    primary: getButtonStyle({
-      textColor: "white",
-      bgColor: "purple-600",
-      hover: "hover:bg-purple-500",
-      hoverRing: "cyan-800",
-      loading: "text-neutral-400 bg-neutral-400 hover:bg-neutral-300",
-    }),
-    secondary: getButtonStyle({
-      textColor: "blue-900",
-      bgColor: "sky-400",
-      hover: "hover:bg-sky-300",
-      hoverRing: "cyan-800",
-      loading: "text-neutral-400 bg-neutral-400 hover:bg-neutral-300",
-    }),
-    ghost: getButtonStyle({
-      textColor: "slate-900",
-      bgColor: "slate-100",
-      hover: "hover:bg-slate-400",
-      hoverRing: "slate-800",
-      loading: "text-neutral-400 bg-neutral-400 hover:bg-neutral-300",
-      custom: `
+    primary: `
+      ${buttonCommon}
+
+      text-white  
+
+      bg-purple-600 
+      hover:bg-purple-500 
+
+      focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 
+
+      border-0 rounded-xl 
+      focus-visible:ring-cyan-800 
+
+      active:ring-2 ring-cyan-800
+    `,
+    secondary: `
+      ${buttonCommon}
+
+      text-blue-900  
+
+      bg-sky-400 
+      hover:bg-sky-300
+
+      focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 
+
+      border-0 rounded-xl 
+      focus-visible:ring-cyan-800 
+
+      active:ring-2 ring-cyan-800
+    `,
+    ghost: `
+      ${buttonCommon}
+
+      text-slate-900  
+
+      bg-slate-100
+      hover:bg-slate-400
+
+      focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 
+
+      border-0 rounded-xl 
+      focus-visible:ring-slate-800 
+
+      active:ring-2 ring-slate-800
+
       shadow-[inset_0_0_0_1px_rgba(0,0,0,0.1)]
       active:outline outline-offset-0 active:outline-slate-500
-      `,
-    }),
-    link: getButtonStyle({
-      textColor: colors.primary,
-      bgColor: colors.bgPrimary,
-      hover: `hover:bg-cyan-300 hover:shadow-cyan-400/500 hover:shadow-lg`,
-      hoverRing: "slate-100",
-      loading: "text-neutral-400 bg-neutral-400 hover:bg-neutral-300",
-      custom: `hover:backdrop-cyan-sm rounded-xl px-5 py-2 sm:px-6`,
-    }),
+    `,
+    link: `
+      ${buttonCommon}
+
+      text-${colors.primary}  
+
+      bg-${colors.bgPrimary} 
+      hover:bg-cyan-300 hover:shadow-cyan-400/500 hover:shadow-lg
+
+      focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 
+
+      border-0 rounded-xl 
+      focus-visible:ring-slate-100 
+
+      active:ring-2 ring-slate-100
+
+    hover:backdrop-cyan-sm rounded-xl px-5 py-2 sm:px-6
+  `,
   },
   table: {
     cell:
